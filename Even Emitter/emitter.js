897d47f62e6dfb0eae95f11a675b7ab2c5ebbb00
Yin <67538811+YinHk@users.noreply.github.com>
2021-11-22 16:00:56 +0800

    Update emitter.js

--- Even Emitter/emitter.js
+++ Even Emitter/emitter.js
@@ -1,1 +1,14 @@
+import EventEmitter from 'eventemitter3';
 
+const eventEmitter = new EventEmitter();
+
+const emitter = {
+  on: (event, fn) => eventEmitter.on(event, fn),
+  once: (event, fn) => eventEmitter.once(event, fn),
+  off: (event, fn) => eventEmitter.off(event, fn),
+  emit: (event, payload) => eventEmitter.emit(event, payload)
+};
+
+Object.freeze(emitter);
+
+export default emitter;
